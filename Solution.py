4e04e440425566d7c227067eea2e37b95eb3db0d
Mohamed Alahmady <mohamedalahmady614@gmail.com>
2023-07-01 22:13:02 +0300

    just for checkout

--- Solution.py
+++ Solution.py
@@ -5,4 +5,7 @@ from typing import List
 def Solve_Linear_Matrix(Y: np.ndarray, J: np.ndarray) -> List:
     Y = Y[1:len(Y), 1:len(Y)]
     J = J[1:len(J)]
+    print(Y)
+    print("")
+    print(J)
     return np.linalg.solve(Y, J)
--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pprint
-
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from File_fun import read_file
 from Parsing_fun import parser
-from Simulations import matrix_formulation_OP,matrix_formulation_AC
+from Simulations import matrix_formulation_OP, matrix_formulation_AC
 from Solution import Solve_Linear_Matrix
 
 
-Circuit_Matrix = parser(read_file('Netlist_1.txt', list))
-Y, J = matrix_formulation_OP(Circuit_Matrix)
-print(Solve_Linear_Matrix(Y, J))
+Circuit_Matrix = parser(read_file('Netlist_2.txt', list))
+# Y, J = matrix_formulation_OP(Circuit_Matrix)
+# print(Solve_Linear_Matrix(Y, J))
 
-# v = []
-# for W in range(1,20):
-#     Y, J = matrix_formulation_AC(Circuit_Matrix,W)
-#     v.append(Solve_Linear_Matrix(Y, J))
+n = Circuit_Matrix["num_nets"]
+v = np.zeros([n, 19])
+print(v.shape)
+for W in range(1, 20):
+    Y, J = matrix_formulation_AC(Circuit_Matrix, W)
+    # v.append(Solve_Linear_Matrix(Y, J))
+    v[:, W] = Solve_Linear_Matrix(Y, J)
 
-# print(v)
+# plt.plot(np.abs(v), range(1, 20))
+
+print(v)
+print(len(v))
